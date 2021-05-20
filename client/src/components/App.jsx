e5b4e8fc98581bd688ff53846ae7bedd72be6d6b
OmoruyiOhuoba <omoruyiohuoba@yahoo.com>
2021-05-21 00:50:33 +0100

    back end update

---
+++ client/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../App.css";
+import {Router, Route, Switch} from "react-router-dom";
+import history from "../history";
+
+import Home from "./Home";
+import Directory from "./Directory";
+import Gallery from "./Gallery";
+import Articles from "./Articles";
+import Awards from "./Awards";
+import Archives from "./Archives"
+import Contact from "./Contact";
+import NavBar from "./Layout/NavBar";
+import Footer from "./Layout/Footer";
+
+
+
