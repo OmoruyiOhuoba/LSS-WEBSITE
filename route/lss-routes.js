30f2d45503e9c6956c85fcbdf083b179636adca1
OmoruyiOhuoba <omoruyiohuoba@yahoo.com>
2021-05-20 13:21:52 +0100

    back end update

--- route/lss-routes.js
+++ route/lss-routes.js
@@ -29,12 +29,12 @@ router.route("/contact").post((req, res, next) => {
     var name = req.body.name
     var email = req.body.email
     var message = req.body.message
-    var content = `name: ${name} \n email: ${email} \n message: ${message} `
+    var content = `WEBSITE CONTACT-US INFO FROM USER\n\n\n\nName: ${name}\n\nEmail: ${email}\n\nMessage: ${message} `
   
     var mail = {
       from: "omoruyiwebsitebookings@gmail.com", 
       to: "omoruyiwebsitebookings@gmail.com, omoruyi_website_bookings@yahoo.com, omoruyiohuoba@yahoo.com, davidngozi2000@yahoo.com", 
-      subject: "WEBSITE BOOKING DETAILS FROM CLIENT",
+      subject: "WEBSITE CONTACT-US INFO FROM USER",
       text: content
     }
   
