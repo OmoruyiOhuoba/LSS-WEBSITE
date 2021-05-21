fe6d1b209a1e76d10418719c571bd8c60474e64a
OmoruyiOhuoba <omoruyiohuoba@yahoo.com>
2021-05-21 08:58:13 +0100

    front end update

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -13,5 +13,29 @@ import Contact from "./Contact";
 import NavBar from "./Layout/NavBar";
 import Footer from "./Layout/Footer";
 
+const App = () =>{
+    return(
+        <div>
+            <Router>
+                <NavBar /> 
+                
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/home" component={Home}/>
+                    <Route path="/directory" component={Directory} />
+                    <Route path="/gallery" component={Gallery} />
+                    <Route path="/articles" component={Articles}/>
+                    <Route path="/archives" component={Archives}/>
+                    <Route path="/awards" component={Awards}/>
+                    <Route path="/contact" component={Contact}/>
+                </Switch>
+                
+                <Footer />
+            </Router>
+        </div>
+     
+    )
+}
 
+export default App;
 
