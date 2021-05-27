3d04e705b552c5fd54feb796b0f733d78f824c93
OmoruyiOhuoba <omoruyiohuoba@yahoo.com>
2021-05-27 10:26:20 +0100

    contact page update

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
+import NavBar from "./Layout/NavBar";
+import axios from "axios";
 
-const Contact = () =>{
-    return(
-        <div>
-
-        </div>
-    );
-}
-
-export default Contact;
+const Contact = () => {
+    
+    const [post, setPost] = useState({
+        name: "",
+        email: "",
+        message:""
+    });
