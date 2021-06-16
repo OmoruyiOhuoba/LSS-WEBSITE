a52c9c0e0cca506c45cf20069bbe6bfb258b49e7
OmoruyiOhuoba <omoruyiohuoba@yahoo.com>
2021-06-16 05:57:25 +0100

    post deployment update

--- client/src/components/HonorRole.jsx
+++ client/src/components/HonorRole.jsx
@@ -3,6 +3,14 @@ import p1 from "../assets/p1.jpeg";
 import p2 from "../assets/p2.jpeg";
 import p3 from "../assets/p3.jpeg";
 import p4 from "../assets/p4.jpeg";
+import p5 from "../assets/p5.jpeg";
+import p6 from "../assets/p6.jpeg";
+import p7 from "../assets/p7.jpeg";
+import p8 from "../assets/p8.jpeg";
+import p9 from "../assets/p9.jpeg";
+import p10 from "../assets/p10.jpeg";
+import p11 from "../assets/p11.jpeg";
+import p12 from "../assets/p12.jpeg";
 
 
 
@@ -20,35 +28,153 @@ const HonourRole = () => {
             <br />
 
             <div className="gossip">
-            <br/>
-            <br/>
-            <br/>
-            <img className="newsImg cartoon" src={p4} alt="law "/>
-            <div className="leader">
+                <br/>
+                <br/>
+                <br/>
+                <img className="newsImg cartoon" src={p4} alt="law "/>
+                <div className="leader">
+                    <h2>2015/2016 Judge & 2016/2017 Chief Judge</h2>
+                    <p className="leader-p"> Usifo Emoyon-Iredia</p>
+                    <p className="leader-p"> LinkedIn: Usifo Emoyon-Iredia </p>
+                </div>
+            </div>
 
-           
-        <h2>2015/2016 Judge & 2016/2017 Chief Judge</h2>
-        <p className="leader-p"> Usifo Emoyon-Iredia</p>
-        <p className="leader-p"> LinkedIn: Usifo Emoyon-Iredia </p>
-
-</div>
+            <div className="gossip">
+                <br/>
+                <br/>
+                <br/>
+                <img className="gossipImg cartoon" src={p2} alt="law "/>
+                <div className="leader">
+                    <h2>2016/2017 Speaker</h2>
+                    <p className="leader-p">Obinna Ekomaru Esq</p>
+                </div>
         </div>
 
-   
-        
-     
+             <div className="gossip">
+                <br/>
+                <br/>
+                <br/>
+                <img className="newsImg cartoon" src={p4} alt="law "/>
+                <div className="leader">
+                    <h2>2015/2016 Judge & 2016/2017 Chief Judge</h2>
+                    <p className="leader-p"> Usifo Emoyon-Iredia</p>
+                    <p className="leader-p"> LinkedIn: Usifo Emoyon-Iredia </p>
+                </div>
+            </div>
+
+            <div className="gossip">
+                <br/>
+                <br/>
+                <br/>
+                <img className="gossipImg cartoon" src={p2} alt="law "/>
+                <div className="leader">
+                    <h2>2016/2017 Speaker</h2>
+                    <p className="leader-p">Obinna Ekomaru Esq</p>
+                </div>
+        </div>
+
+        /* START here*/ 
+
         <div className="gossip">
-            <br/>
-            <br/>
-            <br/>
-            <img className="gossipImg cartoon" src={p2} alt="law "/>
-            <div className="leader">
+                <br/>
+                <br/>
+                <br/>
+                <img className="newsImg cartoon" src={p5} alt="law "/>
+                <div className="leader">
+                    <h2>2017/2018 President</h2>
+                    <p className="leader-p"> Ekpah Martins</p>
+                    <p className="leader-p"> Instagram; martins_fedoje, Twitter: Fedoje_ </p>
+                </div>
+            </div>
 
-           
-        <h2>2016/2017 Speaker</h2>
-        <p className="leader-p">Obinna Ekomaru Esq</p>
-</div>
+            <div className="gossip">
+                <br/>
+                <br/>
+                <br/>
+                <img className="gossipImg cartoon" src={p6} alt="law "/>
+                <div className="leader">
+                    <h2>2019/2020 Chief Judge</h2>
+                    <p className="leader-p">Aigbe Halima Amalohi</p>
+                    <p className="leader-p">LinkedIn Profile- Halima Aigbe, Instagram- leemah_ama</p>
+
+                </div>
         </div>
+
+        <div className="gossip">
+                <br/>
+                <br/>
+                <br/>
+                <img className="newsImg cartoon" src={p7} alt="law "/>
+                <div className="leader">
+                    <h2>2015/2016 Chief Judge</h2>
+                    <p className="leader-p"> Bolloite Deborah Offor</p>
+                    <p className="leader-p"> LinkedIn: Bolloite Deborah Offor</p>
+                </div>
+            </div>
+
+            <div className="gossip">
+                <br/>
+                <br/>
+                <br/>
+                <img className="gossipImg cartoon" src={p2} alt="law "/>
+                <div className="leader">
+                    <h2>2016/2017 Speaker</h2>
+                    <p className="leader-p">Obinna Ekomaru Esq</p>
+                </div>
+        </div>
+
+        <div className="gossip">
+                <br/>
+                <br/>
+                <br/>
+                <img className="newsImg cartoon" src={p4} alt="law "/>
+                <div className="leader">
+                    <h2>2015/2016 Judge & 2016/2017 Chief Judge</h2>
+                    <p className="leader-p"> Usifo Emoyon-Iredia</p>
+                    <p className="leader-p"> LinkedIn: Usifo Emoyon-Iredia </p>
+                </div>
+            </div>
+
+            <div className="gossip">
+                <br/>
+                <br/>
+                <br/>
+                <img className="gossipImg cartoon" src={p2} alt="law "/>
+                <div className="leader">
+                    <h2>2016/2017 Speaker</h2>
+                    <p className="leader-p">Obinna Ekomaru Esq</p>
+                </div>
+        </div>
+
+        <div className="gossip">
+                <br/>
+                <br/>
+                <br/>
+                <img className="newsImg cartoon" src={p4} alt="law "/>
+                <div className="leader">
+                    <h2>2015/2016 Judge & 2016/2017 Chief Judge</h2>
+                    <p className="leader-p"> Usifo Emoyon-Iredia</p>
+                    <p className="leader-p"> LinkedIn: Usifo Emoyon-Iredia </p>
+                </div>
+            </div>
+
+            <div className="gossip">
+                <br/>
+                <br/>
+                <br/>
+                <img className="gossipImg cartoon" src={p2} alt="law "/>
+                <div className="leader">
+                    <h2>2016/2017 Speaker</h2>
+                    <p className="leader-p">Obinna Ekomaru Esq</p>
+                </div>
+        </div>
+
+
+        /*stop here*/
+
+
+
+
         <div className="news">
             <img className="newsImg cartoon" src={p1} alt="logo "/>
             <div className="leader">
